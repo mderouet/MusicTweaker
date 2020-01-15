1700608ebf87607756df8759b514f2c6feb31c00
mderouet <martin.derouet0@gmail.com>
2020-01-16 00:07:04 +0100

    renaming files, fix bugs, audio conversion for mp3

--- tweaker.py
+++ tweaker.py
@@ -1,7 +1,9 @@
 from mp3_tagger import MP3File, VERSION_1, VERSION_2, VERSION_BOTH
 from pydub import AudioSegment
-import datetime, os, eyed3, sys
-
+from pathlib import Path
+import datetime, os, eyed3, sys, requests, random, os.path
+from pyquery import PyQuery
+from bs4 import BeautifulSoup
 
 pathToMusicFolder = 'musics/'
 musics = []
@@ -24,25 +26,31 @@ clear()
 displayBanner('MUSIC TWEAKER', 1)
 
 
-
-for filename in os.listdir(pathToMusicFolder):
-    if not filename.endswith(".DS_Store"): 
-        musics.append(filename)
+def scanFolder():
+    musics.clear()
+    for filename in os.listdir(pathToMusicFolder):
+        if not filename.endswith(".DS_Store"):
+            musics.append(filename)
+    musics.sort()
 
 
 def clearTags():
-    emptyPrint(2)
     displayBanner('REMOVE METADATA')
-    emptyPrint(3)
-    artistName = input("Artist name : ")
-    albumTitle = input("Album title : ")
+    onlyMp3 = True
+    for music in musics:
+        if not music.lower().endswith('.mp3'):
+            onlyMp3 = False
+            print('Can only clear tags of mp3 files, convert first audio files to mp3')
+            return
+
+    emptyPrint(2)
     for currentMusic in musics:
 
         mp3 = MP3File(pathToMusicFolder + currentMusic)
 
         del mp3.song
-        mp3.artist = artistName
-        mp3.album = albumTitle
+        del mp3.artist
+        del mp3.album
         mp3.year = str(datetime.datetime.now().year)
         del mp3.comment
         del mp3.track
@@ -59,8 +67,11 @@ def clearTags():
         tags = mp3.get_tags()
 
         print(currentMusic + ' tags removed')
-    #print(tags)
-    return
+
+    emptyPrint(2)
+    choice = input('Would you like to display logs of operation ? Y/N \n')
+    if choice.lower() == 'y':
+        print(tags)
 
 
 def emptyPrint(number):
@@ -74,20 +85,27 @@ def audioFormatConversion():
     print('Convert everything to :')
     print()
     print('    1. Mp3')
-    print('    2. WAV')
-    print('    3. Flac')
+    #print('    2. WAV')
+    #print('    3. Flac')
     print()
-    choiceFormat = int(input('Choice : \n'))
+    choiceFormat = input('Choice : \n')
     emptyPrint(2)
-    if choiceFormat == 1:
+    if int(choiceFormat) == 1:
         for music in musics:
             if music.lower().endswith('.wav'):
                 print('Converting ' + music + ' to Mp3 format')
-                wav_audio = AudioSegment.from_file(pathToMusicFolder + music, format="wav")
-                wav_audio.export("test.mp3", format="mp3")
+                raw_audio = AudioSegment.from_file(pathToMusicFolder + music, format="raw",frame_rate=44100, channels=2, sample_width=2)
+                raw_audio.export(pathToMusicFolder + music.lower().replace('.wav', '.mp3'), format="mp3", bitrate="320k")
+                os.remove(pathToMusicFolder + music)
             if music.lower().endswith('.flac'):
                 print('Converting ' + music + ' to Mp3 format')
-        return
+                flac_audio = AudioSegment.from_file(pathToMusicFolder + music, "flac")
+                flac_audio.export(pathToMusicFolder + music.lower().replace('.flac', '.mp3'), format="mp3", bitrate="320k")
+                os.remove(pathToMusicFolder + music)
+            if music.lower().endswith('.mp3'):
+                print('Converting ' + music + ' to Mp3 format')
+                raw_audio = AudioSegment.from_file(pathToMusicFolder + music, format="raw",frame_rate=44100, channels=2, sample_width=2)
+                raw_audio.export(pathToMusicFolder + music, format="mp3", bitrate="320k")
     if choiceFormat == 2:
         print('2')
     if choiceFormat == 3:
@@ -110,7 +128,29 @@ def removeAlbumCover():
     if choice.lower() == 'y':
         print(logs)
 
+def renameFiles():
+    songs = []
+    for ms in musics:
+        if(len(songs) == 0):
+            print('Requesting for new song titles...')
+            try:
+                response = requests.get('https://learnhowtowritesongs.com/wp-content/plugins/song-title-generator/title-function.php')
+            except:
+                print("Cannot reach learnhowtowritesongs.com, check your internet connection and whether the site is online")
+                return
+            soup = BeautifulSoup(response.text, 'html.parser')
+            html = soup.find('div', id='songTitle').get_text()
+            for line in html.splitlines():
+                if len(line) > 4:
+                    songs.append(line)
+
+        randomSongName = random.choice(songs)
+        songs.remove(randomSongName)
+        os.rename(pathToMusicFolder + ms, pathToMusicFolder + randomSongName + os.path.splitext(ms)[1])
+        print("Renamed file : " + ms + " to " + randomSongName + os.path.splitext(ms)[1])
+
 def menu():
+    scanFolder()
     emptyPrint(2)
     print('MENU :')
     print()
@@ -118,7 +158,14 @@ def menu():
     print('    2. Remove metadata')
     print('    3. Remove album cover')
     print('    4. Change audio format')
-    print('    5. Exit')
+    print('    5. Change file name')
+    print('    6. Exit')
+
+    try:
+        response = requests.get('https://google.fr/')
+    except:
+        print("We detected an issue with your internet connection, some features will not work properly")
+
     emptyPrint(2)
     choice = int(input('Choice : '))
     print(choice)
@@ -135,7 +182,9 @@ def menu():
         clear()
         audioFormatConversion()
     if choice == 5:
+        clear()
+        renameFiles()
+    if choice == 6:
         sys.exit()
     menu()
-
 menu()
